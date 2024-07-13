e80468ab3aa4c34357a6240da031456ac7941fd1
Sofia <sofiaescajadillob@gmail.com>
2024-07-13 18:03:44 -0500

    Creación de proyecto
    Se creó el proyecto en Netbeans y se actualizó el .gitignore

---
+++ Conversor de monedas/src/com/alura/conversor/principal/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.alura.conversor.principal;
+
+/**
+ *
+ * @author sofia
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
